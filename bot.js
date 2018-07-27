ed5efd4a7acba8f65a85d37ce4c673d2609c094d
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-07-27 19:36:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,30 @@
-const Discord = require('discord.js');
-const A7MD = new Discord.Client();
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "$";
+client.on("message", message => {
 
-console.log("BOT ONLINE");
- 
-A7MD.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
-**
-hello may I get to know you
-By this server :rose: 
-  [ https://discord.gg/DeSUK8E ]
- 
-Special invitation to you... [ ${member}  ]
-**`) 
-}).catch(console.error)
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
 })
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
 
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`#bc | Attiyah`,"")
+client.user.setStatus("dnd")
+});
 
 client.login(process.env.BOT_TOKEN); 
