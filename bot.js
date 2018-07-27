b4ba74f5c0033190002d8fc35b47cb5c98f2b565
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-07-27 19:13:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,30 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
- const prefix = "برفكسك";
-client.on('ready', () => {
-    console.log('I am ready!');
+var prefix = "$";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
 });
 
-client.on('message', message => {
-    if (message.content === 'zg') {
-        message.reply('pong');
-      }
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`#bc | Attiyah`,"")
+client.user.setStatus("dnd")
 });
 
 
